cecc485a6aad3639500e61c44d1af656fe6ae8d0
George Bixby <george@chartbeat.com>
2017-07-31 11:58:45 -0400

    really bad html/css prototype

--- capitolwords_ng/frontend/capitolwords-spa/src/components/HeaderBar/HeaderBar.js
+++ capitolwords_ng/frontend/capitolwords-spa/src/components/HeaderBar/HeaderBar.js
@@ -12,13 +12,52 @@ class HeaderBar extends Component {
 
   render() {
     return (
-      <div className="HeaderBar-container">
-        <div>
-          <h1 className="HeaderBar-title">Capitol Words</h1>
-          <div className="HeaderBar-tagline">See what congress is talking about</div>
+      <div>
+        <div className="HeaderBar-container">
+          <div>
+            <h1 className="HeaderBar-title">Capitol Words</h1>
+          </div>
+          <div className="headerBarSearch">
+            <SearchInput onSubmit={this.props.onSearchSubmit}/>
+          </div>
         </div>
-        <div>
-          <SearchInput onSubmit={this.props.onSearchSubmit}/>
+
+        <div className="SearchResults">
+          <div className="ResultsHeader">
+            <span className="TextSmall">Search results for:</span>
+            <h1>Education</h1>
+          </div>
+          <div className="DateToggle">
+            <ul>
+              <li className="TabActive">30 days</li>
+              <li>3 months</li>
+              <li>6 months</li>
+            </ul>
+          </div>
+          <div className="TopMetricsList">
+            <div className="MetricListItem">
+              <h2>261</h2>
+              <span>Total Mentions</span>
+            </div>
+            <div className="MetricListItem">
+              <h2>+2%</h2>
+              <span>Compared to previous 30 days</span>
+            </div>
+          </div>
+
+          <div className="TopRecords">
+            <h3>Records with the most mentions</h3>
+            <div className="Record">
+              <div className="Date">
+                May 24, 2017
+              </div>
+              <h4 className="RecordTitle">Education Funding in the President’s Budget</h4>
+              <span className="MentionCount">3 mentions</span>
+              <blockquote>“This is why I was shocked the President's budget makes drastic cuts to <span className="Highlight">education</span>—cuts that will have the biggest impact on kids who need our help the most.”</blockquote>
+              <div class="SentorAvatar"></div>
+            </div>
+
+          </div> 
         </div>
       </div>
     );
