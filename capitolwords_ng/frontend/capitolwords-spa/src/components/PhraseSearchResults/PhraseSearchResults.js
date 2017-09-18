e21e3c91dd372cc43a15015c411b60ad803419e5
will-horning <will.horning@gmail.com>
2017-09-18 12:19:26 -0400

    [frontend] lotsa features, lotsa ugly-ass code

--- capitolwords_ng/frontend/capitolwords-spa/src/components/PhraseSearchResults/PhraseSearchResults.js
+++ capitolwords_ng/frontend/capitolwords-spa/src/components/PhraseSearchResults/PhraseSearchResults.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Highlighter from 'react-highlight-words'
 
 import './PhraseSearchResults.css';
 
@@ -11,6 +12,7 @@ import {
   searchResultCount,
   searchContent,
   searchResultList,
+  searchDelta
 } from '../../selectors/phrase-search-selectors';
 
 class PhraseSearchResults extends Component {
@@ -20,14 +22,51 @@ class PhraseSearchResults extends Component {
     isSearchSuccess: PropTypes.bool.isRequired,
     searchResultCount: PropTypes.number,
     searchResultList: PropTypes.array,
-    searchContent: PropTypes.string
+    searchContent: PropTypes.string,
+    searchDelta: PropTypes.number
   };
 
+  renderSpeakerThumb(item){
+    return (
+      <img className="PhraseSearchResults-speaker-im-r" src={ item.im_url } alt={ item.im_url } />
+    );
+  }
+
   renderResultItem(item) {
+    // var searchTerms = [searchContent];
+    // var images = [];
+    // for (var i=0; i < item.speakers.length; i++) {
+    //   if (item.party === "Republican") {
+    //      images.push(<img key={item.im_url} className="PhraseSearchResults-speaker-im-r" src={ item.im_url } alt={ item.im_url } />);
+    //   }
+    //   else if (item.party === "Democrat") {
+    //      images.push(<img key={item.im_url} className="PhraseSearchResults-speaker-im-d" src={ item.im_url } alt={ item.im_url } />);
+    //   }
+    //   else {
+    //      images.push(<img key={item.im_url} className="PhraseSearchResults-speaker-im-i" src={ item.im_url } alt={ item.im_url } />);
+    //   }
+    // }
     return (
       <li className="PhraseSearchResults-item" key={item._id}>
-        <div className="PhraseSearchResults-item-id">{ item._source.ID }</div>
+        <div className="PhraseSearchResults-item-date">
+          { item.human_date }
+        </div>
+        <div className="PhraseSearchResults-item-mention-count">{ item.mentions } mentions</div>
         <a href={ item._source.html_url } className="PhraseSearchResults-item-title">{ item._source.title }</a>
+        <div className="PhraseSearchResults-item-snippet">
+          <Highlighter
+            highlightClassName="snippetHighlight"
+            searchWords={[item.search_phrase]}
+            textToHighlight={ item.snippet }
+          />
+        </div>
+        { item.speakers.map(function (speaker){
+          return (
+            <a href={ speaker.bio_page_url }>
+            <img data-party={speaker.party} className="PhraseSearchResults-speaker-im" src={ speaker.im_url } alt={ speaker.im_url } />
+            </a>
+          );
+          })}
       </li>
     );
   }
@@ -48,7 +87,8 @@ class PhraseSearchResults extends Component {
       isSearchFailure,
       isSearchSuccess,
       searchResultCount,
-      searchContent
+      searchContent,
+      searchDelta
     } = this.props;
 
     if (isSearching) {
@@ -62,25 +102,26 @@ class PhraseSearchResults extends Component {
     if (isSearchSuccess) {
       return (
         <div>
+
           <div className="PhraseSearchResults-results-for">Search results for:</div>
           <div className="PhraseSearchResults-phrase"> {searchContent} </div>
           <div className="PhraseSearchResults-date-selector">
             30 days | 3 months | 6 months
           </div>
           <div className="PhraseSearchResults-metrics-container">
-            <div className="PhraseSearchResults-results-count-metric">
-              <div className="PhraseSearchResults-metric-value">
+            <div className="PhraseSearchResults-metric-box">
+              <div className="PhraseSearchResults-metric-box-value">
                 { searchResultCount }
               </div>
-              <div className="PhraseSearchResults-metric-name">
+              <div className="PhraseSearchResults-metric-box-label">
                 Total Mentions
               </div>
             </div>
-            <div className="PhraseSearchResults-results-benchmark">
-              <div className="PhraseSearchResults-metric-value">
-                +10%
+            <div className="PhraseSearchResults-metric-box">
+              <div className="PhraseSearchResults-metric-box-value">
+                 { searchDelta }%
               </div>
-              <div className="PhraseSearchResults-metric-name">
+              <div className="PhraseSearchResults-metric-box-label">
                 Compared to previous 30 days
               </div>
             </div>
@@ -109,6 +150,7 @@ export default connect(state => ({
   isSearchFailure: isSearchFailure(state),
   isSearchSuccess: isSearchSuccess(state),
   searchContent: searchContent(state),
+  searchDelta: searchDelta(state),
   searchResultCount: searchResultCount(state),
   searchResultList: searchResultList(state),
 }))(PhraseSearchResults);
